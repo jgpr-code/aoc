7291877d756b020103a4cef68a60f3eb905f1196
Jörg Planner <joerg.planner@methodpark.de>
2023-10-24 10:01:03 +0200

    [2022/rust] Fiddle with clap and solution setup

--- 2022/rust/aoc/src/main.rs
+++ 2022/rust/aoc/src/main.rs
@@ -1,14 +1,63 @@
+use clap::Parser;
+use std::collections::HashMap;
+
 mod day01;
 mod day02;
 
+#[derive(Debug, Parser)]
+struct Cli {
+    day: Option<u8>,
+    part: Option<u8>,
+}
+
+struct Solver {
+    solutions: HashMap<(u8, u8), fn()>,
+}
+
+impl Solver {
+    fn new() -> Solver {
+        Solver {
+            solutions: HashMap::new(),
+        }
+    }
+    fn add(&mut self, day: u8, part: u8, fun: fn()) {
+        self.solutions.insert((day, part), fun);
+    }
+    fn solve(&self, day: &Option<u8>, part: &Option<u8>) {}
+    fn solve_day_part(&self, day: u8, part: u8) {}
+    fn solve_day(&self, day: u8) {}
+    fn solve_part(&self, part: u8) {}
+    fn solve_all(&self) {}
+}
+
 fn main() {
+    let cli = Cli::parse();
+
+    let mut solver = Solver::new();
+    solver.add(1, 1, day01::say_hello);
+    run(&cli.day, &cli.part);
     println!("Hello, world!");
     day01::say_hello();
     //day01::say_goodbye(); not possible -> private function
     day02::say_hello();
 }
 
+fn run(day: &Option<u8>, part: &Option<u8>) {
+    // no day, no part -> run all
+    // no day, part -> run that part for all days
+    // day, no part -> run parts for that day
+    // day, part -> run that part for that day
+    match (day, part) {
+        (Some(day), Some(part)) => println!("day {} part {} is not implemented yet", day, part),
+        (None, None) => todo!(),
+        (None, Some(_)) => todo!(),
+        (Some(_), None) => todo!(),
+    }
+}
+
 // use cases
+
+// cargo run r 1 1 -> 'r' for run '1' for day01 '1' for part1
 
 // run/test day part 1
 // run/test day part 2
