d714aba7b7e8f28a953e10cec48ce91fce7b7b9c
Jörg Planner <joerg.planner@methodpark.de>
2023-12-01 18:07:33 +0100

    [2023/rust] move Solver to common module

--- 2023/rust/src/common/mod.rs
+++ 2023/rust/src/common/mod.rs
@@ -1,4 +1,6 @@
+use anyhow::Result;
 use std::fmt::Display;
+use std::{collections::HashMap, fs, path::PathBuf};
 
 // the return type for parts sometime its Numbers sometimes its Strings
 #[derive(Debug, PartialEq, Eq)]
@@ -13,6 +15,64 @@ impl Display for Answer {
         match &self {
             Answer::Num(i) => write!(f, "{}", i),
             Answer::Str(s) => write!(f, "{}", s),
+        }
+    }
+}
+
+pub struct Solver {
+    solutions: HashMap<(u8, u8), fn(&str) -> Result<Answer>>,
+}
+
+impl Solver {
+    pub fn new() -> Solver {
+        Solver {
+            solutions: HashMap::new(),
+        }
+    }
+    pub fn add(&mut self, day: u8, part: u8, fun: fn(&str) -> Result<Answer>) {
+        self.solutions.insert((day, part), fun);
+    }
+    pub fn solve(&self, opt: crate::Opt) {
+        match opt {
+            crate::Opt::All => self.solve_all(),
+            crate::Opt::Day { day, part } => self.solve_day(day, part),
+            crate::Opt::File { day, part, file } => self.solve_day_part_file(day, part, file),
+        }
+    }
+    fn solve_all(&self) {
+        for i in 1..=25 {
+            self.solve_day(i, None);
+        }
+    }
+    fn solve_day(&self, day: u8, part: Option<u8>) {
+        let file = format!("src/day{:02}/input.txt", day);
+        match fs::read_to_string(&file) {
+            Ok(content) => {
+                if let Some(part) = part {
+                    self.solve_day_part_content(day, part, &content)
+                } else {
+                    self.solve_day_part_content(day, 1, &content);
+                    self.solve_day_part_content(day, 2, &content);
+                }
+            }
+            Err(err) => println!("Error reading {}: {}", file, err),
+        }
+    }
+    fn solve_day_part_file(&self, day: u8, part: u8, file: PathBuf) {
+        match fs::read_to_string(&file) {
+            Ok(content) => self.solve_day_part_content(day, part, &content),
+            Err(err) => println!("Error reading {}: {}", file.display(), err),
+        }
+    }
+    fn solve_day_part_content(&self, day: u8, part: u8, content: &str) {
+        print!("day{:02} part{:02}: ", day, part);
+        if let Some(fun) = self.solutions.get(&(day, part)) {
+            match fun(content) {
+                Ok(answer) => println!("{}", answer),
+                Err(err) => println!("Err: implementation failed with: {}", err),
+            }
+        } else {
+            println!("Err: No solution was added to solver!");
         }
     }
 }
--- 2023/rust/src/main.rs
+++ 2023/rust/src/main.rs
@@ -5,9 +5,8 @@ mod common;
 mod day00;
 mod day01;
 
-use anyhow::Result;
-use common::Answer;
-use std::{collections::HashMap, fs, path::PathBuf};
+use common::*;
+use std::path::PathBuf;
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
@@ -26,62 +25,4 @@ fn main() {
     solver.add(1, 1, day01::part_one);
     solver.add(1, 2, day01::part_two);
     solver.solve(opt);
-}
-
-struct Solver {
-    solutions: HashMap<(u8, u8), fn(&str) -> Result<Answer>>,
-}
-
-impl Solver {
-    fn new() -> Solver {
-        Solver {
-            solutions: HashMap::new(),
-        }
-    }
-    fn add(&mut self, day: u8, part: u8, fun: fn(&str) -> Result<Answer>) {
-        self.solutions.insert((day, part), fun);
-    }
-    fn solve(&self, opt: Opt) {
-        match opt {
-            Opt::All => self.solve_all(),
-            Opt::Day { day, part } => self.solve_day(day, part),
-            Opt::File { day, part, file } => self.solve_day_part_file(day, part, file),
-        }
-    }
-    fn solve_all(&self) {
-        for i in 1..=25 {
-            self.solve_day(i, None);
-        }
-    }
-    fn solve_day(&self, day: u8, part: Option<u8>) {
-        let file = format!("src/day{:02}/input.txt", day);
-        match fs::read_to_string(&file) {
-            Ok(content) => {
-                if let Some(part) = part {
-                    self.solve_day_part_content(day, part, &content)
-                } else {
-                    self.solve_day_part_content(day, 1, &content);
-                    self.solve_day_part_content(day, 2, &content);
-                }
-            }
-            Err(err) => println!("Error reading {}: {}", file, err),
-        }
-    }
-    fn solve_day_part_file(&self, day: u8, part: u8, file: PathBuf) {
-        match fs::read_to_string(&file) {
-            Ok(content) => self.solve_day_part_content(day, part, &content),
-            Err(err) => println!("Error reading {}: {}", file.display(), err),
-        }
-    }
-    fn solve_day_part_content(&self, day: u8, part: u8, content: &str) {
-        print!("day{:02} part{:02}: ", day, part);
-        if let Some(fun) = self.solutions.get(&(day, part)) {
-            match fun(content) {
-                Ok(answer) => println!("{}", answer),
-                Err(err) => println!("Err: implementation failed with: {}", err),
-            }
-        } else {
-            println!("Err: No solution was added to solver!");
-        }
-    }
 }
