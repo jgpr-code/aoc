d55c96b209841bf17fdd605a7a2260febf9e015b
Jörg Planner <joerg.planner@methodpark.de>
2023-12-13 06:43:44 +0100

    [2023/rust] day13 part_two

--- 2023/rust/src/day13/mod.rs
+++ 2023/rust/src/day13/mod.rs
@@ -31,7 +31,7 @@ fn solve_one(input: &Input) -> Result<Answer> {
     let Input { grids } = input;
     let mut sum = 0;
     for grid in grids {
-        let (href, vref) = find_reflection(grid);
+        let (href, vref) = find_reflection(grid, false);
         // 0 indexed, but needs 1
         if href != -1 {
             sum += (href + 1) * 100;
@@ -44,13 +44,18 @@ fn solve_one(input: &Input) -> Result<Answer> {
 }
 
 // convention is to return the last index of the top
-fn find_reflection(grid: &Vec<Vec<char>>) -> (i128, i128) {
+fn find_reflection(grid: &Vec<Vec<char>>, smudge: bool) -> (i128, i128) {
     let mut horizontal = Vec::new();
     for line in grid {
         let lstr = String::from_iter(line.iter());
         horizontal.push(lstr);
     }
-    let href = find_refl(&horizontal);
+
+    let href = if smudge {
+        find_refl_smudge(&horizontal)
+    } else {
+        find_refl(&horizontal)
+    };
 
     let mut vertical = Vec::new();
     assert!(grid.len() > 0);
@@ -61,7 +66,11 @@ fn find_reflection(grid: &Vec<Vec<char>>) -> (i128, i128) {
         }
         vertical.push(cstr);
     }
-    let vref = find_refl(&vertical);
+    let vref = if smudge {
+        find_refl_smudge(&vertical)
+    } else {
+        find_refl(&vertical)
+    };
     assert!(href == -1 || vref == -1);
     (href, vref)
 }
@@ -88,7 +97,60 @@ fn find_refl(line: &Vec<String>) -> i128 {
 }
 
 fn solve_two(input: &Input) -> Result<Answer> {
-    Ok(Answer::Num(todo!()))
+    let Input { grids } = input;
+    let mut sum = 0;
+    for grid in grids {
+        let (href, vref) = find_reflection(grid, true);
+        // 0 indexed, but needs 1
+        if href != -1 {
+            sum += (href + 1) * 100;
+        }
+        if vref != -1 {
+            sum += vref + 1;
+        }
+    }
+    Ok(Answer::Num(sum))
+}
+
+fn find_refl_smudge(line: &Vec<String>) -> i128 {
+    let mut reflect = -1;
+    for try_reflect in 0..line.len() - 1 {
+        let mut li = try_reflect as i128;
+        let mut ri = try_reflect as i128 + 1;
+        let mut success = true;
+        let mut smudge_found = false;
+        while li >= 0 && ri < line.len() as i128 {
+            let diff = count_diff(&line[li as usize], &line[ri as usize]);
+            if diff == 1 {
+                if !smudge_found {
+                    smudge_found = true
+                } else {
+                    success = false;
+                    break;
+                }
+            } else if diff > 1 {
+                success = false;
+                break;
+            }
+            li -= 1;
+            ri += 1;
+        }
+        if success && smudge_found {
+            reflect = try_reflect as i128;
+        }
+    }
+    reflect
+}
+
+fn count_diff(l: &str, r: &str) -> usize {
+    let mut diff = 0;
+    assert_eq!(l.len(), r.len());
+    for (lc, rc) in l.chars().zip(r.chars()) {
+        if lc != rc {
+            diff += 1;
+        }
+    }
+    diff
 }
 
 #[cfg(test)]
@@ -109,19 +171,19 @@ mod tests {
     #[test]
     fn part_one() -> Result<()> {
         let answer = super::part_one(&INPUT)?;
-        assert_eq!(answer, Answer::Num(-1));
+        assert_eq!(answer, Answer::Num(35521));
         Ok(())
     }
     #[test]
     fn test_two() -> Result<()> {
         let answer = super::part_two(&TEST)?;
-        assert_eq!(answer, Answer::Num(todo!()));
+        assert_eq!(answer, Answer::Num(400));
         Ok(())
     }
     #[test]
     fn part_two() -> Result<()> {
         let answer = super::part_two(&INPUT)?;
-        assert_eq!(answer, Answer::Num(todo!()));
+        assert_eq!(answer, Answer::Num(34795));
         Ok(())
     }
 
