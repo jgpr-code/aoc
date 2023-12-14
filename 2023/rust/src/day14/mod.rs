73220157c240773e37aa9f699827b61fb0011a54
Jörg Planner <joerg.planner@methodpark.de>
2023-12-14 08:19:58 +0100

    [2023/rust] day 14 part_two

--- 2023/rust/src/day14/mod.rs
+++ 2023/rust/src/day14/mod.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, ops::Add};
 
 use super::common::*;
 use anyhow::Result;
@@ -46,20 +46,59 @@ fn print_board(board: &Vec<Vec<char>>) {
     }
 }
 
-fn solve_one(input: &Input) -> Result<Answer> {
-    let Input { board } = input;
-    // start with last row and move up
-    // index and current amount of moving rocks
-    let rows = board.len();
-    let cols = board[0].len();
-    let mut new_board = board.clone(); // should be deep copy
-    for r in 0..new_board.len() {
-        for c in 0..new_board[0].len() {
-            if new_board[r][c] == 'O' {
-                new_board[r][c] = '.';
+fn hash_board(board: &Vec<Vec<char>>) -> String {
+    let mut hash = String::new();
+    for r in 0..board.len() {
+        let line = String::from_iter(board[r].iter());
+        hash = format!("{}{}", hash, line);
+    }
+    hash
+}
+
+fn unhash_board(hash: &String, rows: usize, cols: usize) -> Vec<Vec<char>> {
+    let mut board = vec![vec!['.'; cols]; rows];
+    let mut si = 0;
+    let cs: Vec<char> = hash.chars().collect();
+    for r in 0..rows {
+        for c in 0..cols {
+            board[r][c] = cs[si];
+            si += 1;
+        }
+    }
+    board
+}
+
+fn empty_rocks(board: &mut Vec<Vec<char>>) {
+    for r in 0..board.len() {
+        for c in 0..board[0].len() {
+            if board[r][c] == 'O' {
+                board[r][c] = '.';
             }
         }
     }
+}
+
+fn solve_one(input: &Input) -> Result<Answer> {
+    let Input { board } = input;
+    let mut a = HashMap::new();
+    let new_board = tilt_north(board, &mut a);
+    // print_board(board);
+    // println!();
+    // print_board(&new_board);
+    Ok(Answer::Num(compute_board_load(&new_board) as i128))
+}
+
+fn tilt_north(board: &Vec<Vec<char>>, memo: &mut HashMap<String, String>) -> Vec<Vec<char>> {
+    // bottom to top
+    let rows = board.len();
+    let cols = board[0].len();
+    let hash = hash_board(board);
+    if memo.contains_key(&hash) {
+        let hashed_board = memo.get(&hash).unwrap();
+        return unhash_board(hashed_board, rows, cols);
+    }
+    let mut new_board = board.clone(); // should be deep copy
+    empty_rocks(&mut new_board);
     let mut moving_rocks = vec![0; cols];
     for r in (0..rows).rev() {
         // println!("{} {:?}", r, moving_rocks);
@@ -92,15 +131,199 @@ fn solve_one(input: &Input) -> Result<Answer> {
         }
         moving_rocks[c] = 0;
     }
-    print_board(board);
-    println!();
-    print_board(&new_board);
-    Ok(Answer::Num(compute_board_load(&new_board) as i128))
+    let new_hash = hash_board(&new_board);
+    memo.insert(hash, new_hash);
+    new_board
+}
+fn tilt_west(board: &Vec<Vec<char>>, memo: &mut HashMap<String, String>) -> Vec<Vec<char>> {
+    // right to left
+    let rows = board.len();
+    let cols = board[0].len();
+    let hash = hash_board(board);
+    if memo.contains_key(&hash) {
+        let hashed_board = memo.get(&hash).unwrap();
+        return unhash_board(hashed_board, rows, cols);
+    }
+    let mut new_board = board.clone(); // should be deep copy
+    empty_rocks(&mut new_board);
+    let mut moving_rocks = vec![0; rows];
+    for c in (0..cols).rev() {
+        // println!("{} {:?}", r, moving_rocks);
+        for r in 0..rows {
+            match board[r][c] {
+                '#' => {
+                    let mut to_place = moving_rocks[r];
+                    let mut cright = c + 1;
+                    while to_place > 0 && cright < cols {
+                        new_board[r][cright] = 'O';
+                        to_place -= 1;
+                        cright += 1;
+                    }
+                    moving_rocks[r] = 0;
+                }
+                'O' => moving_rocks[r] += 1,
+                _ => {
+                    // println!("ignoring {}", board[r][c])
+                }
+            }
+        }
+    }
+    for r in 0..rows {
+        let mut to_place = moving_rocks[r];
+        let mut cright = 0;
+        while to_place > 0 && cright < cols {
+            new_board[r][cright] = 'O';
+            to_place -= 1;
+            cright += 1;
+        }
+        moving_rocks[r] = 0;
+    }
+    let new_hash = hash_board(&new_board);
+    memo.insert(hash, new_hash);
+    new_board
+}
+fn tilt_south(board: &Vec<Vec<char>>, memo: &mut HashMap<String, String>) -> Vec<Vec<char>> {
+    // top to bottom
+    let rows = board.len();
+    let cols = board[0].len();
+    let hash = hash_board(board);
+    if memo.contains_key(&hash) {
+        let hashed_board = memo.get(&hash).unwrap();
+        return unhash_board(hashed_board, rows, cols);
+    }
+    let mut new_board = board.clone(); // should be deep copy
+    empty_rocks(&mut new_board);
+    let mut moving_rocks = vec![0; cols];
+    for r in 0..rows {
+        // println!("{} {:?}", r, moving_rocks);
+        for c in 0..cols {
+            match board[r][c] {
+                '#' => {
+                    let mut to_place = moving_rocks[c];
+                    let mut rabove = r;
+                    while to_place > 0 && rabove > 0 {
+                        new_board[rabove - 1][c] = 'O';
+                        to_place -= 1;
+                        rabove -= 1;
+                    }
+                    moving_rocks[c] = 0;
+                }
+                'O' => moving_rocks[c] += 1,
+                _ => {
+                    // println!("ignoring {}", board[r][c])
+                }
+            }
+        }
+    }
+    for c in 0..cols {
+        let mut to_place = moving_rocks[c];
+        let mut rabove = rows;
+        while to_place > 0 && rabove > 0 {
+            new_board[rabove - 1][c] = 'O';
+            to_place -= 1;
+            rabove -= 1;
+        }
+        moving_rocks[c] = 0;
+    }
+    let new_hash = hash_board(&new_board);
+    memo.insert(hash, new_hash);
+    new_board
+}
+fn tilt_east(board: &Vec<Vec<char>>, memo: &mut HashMap<String, String>) -> Vec<Vec<char>> {
+    // left to right
+    let rows = board.len();
+    let cols = board[0].len();
+    let hash = hash_board(board);
+    if memo.contains_key(&hash) {
+        let hashed_board = memo.get(&hash).unwrap();
+        return unhash_board(hashed_board, rows, cols);
+    }
+    let mut new_board = board.clone(); // should be deep copy
+    empty_rocks(&mut new_board);
+    let mut moving_rocks = vec![0; rows];
+    for c in 0..cols {
+        // println!("{} {:?}", r, moving_rocks);
+        for r in 0..rows {
+            match board[r][c] {
+                '#' => {
+                    let mut to_place = moving_rocks[r];
+                    let mut cleft = c;
+                    while to_place > 0 && cleft > 0 {
+                        new_board[r][cleft - 1] = 'O';
+                        to_place -= 1;
+                        cleft -= 1;
+                    }
+                    moving_rocks[r] = 0;
+                }
+                'O' => moving_rocks[r] += 1,
+                _ => {
+                    // println!("ignoring {}", board[r][c])
+                }
+            }
+        }
+    }
+    for r in 0..rows {
+        let mut to_place = moving_rocks[r];
+        let mut cleft = cols;
+        while to_place > 0 && cleft > 0 {
+            new_board[r][cleft - 1] = 'O';
+            to_place -= 1;
+            cleft -= 1;
+        }
+        moving_rocks[r] = 0;
+    }
+    let new_hash = hash_board(&new_board);
+    memo.insert(hash, new_hash);
+    new_board
+}
+fn cycle(board: &Vec<Vec<char>>) -> Vec<Vec<char>> {
+    let mut nmap = HashMap::new();
+    let mut wmap = HashMap::new();
+    let mut smap = HashMap::new();
+    let mut emap = HashMap::new();
+    let mut n = tilt_north(board, &mut nmap);
+    n = tilt_west(&n, &mut wmap);
+    n = tilt_south(&n, &mut smap);
+    n = tilt_east(&n, &mut emap);
+    n
 }
 
 fn solve_two(input: &Input) -> Result<Answer> {
-    todo!();
-    Ok(Answer::Num(-1))
+    let Input { board } = input;
+    // let mut n = cycle(board);
+    // println!("one cycle:");
+    // print_board(&n);
+    // n = cycle(&n);
+    // println!("two cycle");
+    // print_board(&n);
+    let mut n = board.clone();
+    let amount_cycles = 1000000000;
+    let mut encountered = HashMap::new();
+    let mut curr = 0;
+    while curr < amount_cycles {
+        let before_hash = hash_board(&n);
+        if encountered.contains_key(&before_hash) {
+            let cycle_dist = curr - encountered.get(&before_hash).unwrap();
+            while curr + cycle_dist < amount_cycles {
+                curr += cycle_dist;
+            }
+        } else {
+            encountered.insert(before_hash.clone(), curr);
+        }
+
+        n = cycle(&n);
+        let after_hash = hash_board(&n);
+        if before_hash == after_hash {
+            break; //stale now
+        }
+        curr += 1;
+        // if i < 3 {
+        //     println!("{}", i);
+        //     print_board(&n);
+        //     println!();
+        // }
+    }
+    Ok(Answer::Num(compute_board_load(&n) as i128))
 }
 
 #[cfg(test)]
@@ -121,19 +344,19 @@ mod tests {
     #[test]
     fn part_one() -> Result<()> {
         let answer = super::part_one(&INPUT)?;
-        assert_eq!(answer, Answer::Num(-1));
+        assert_eq!(answer, Answer::Num(108826));
         Ok(())
     }
     #[test]
     fn test_two() -> Result<()> {
         let answer = super::part_two(&TEST)?;
-        assert_eq!(answer, Answer::Num(-1));
+        assert_eq!(answer, Answer::Num(64));
         Ok(())
     }
     #[test]
     fn part_two() -> Result<()> {
         let answer = super::part_two(&INPUT)?;
-        assert_eq!(answer, Answer::Num(-1));
+        assert_eq!(answer, Answer::Num(99291));
         Ok(())
     }
 
