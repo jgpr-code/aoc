96dabb364d1136a31b12f5bc8c874a804678ebb4
Jörg Planner <joerg.planner@methodpark.de>
2023-12-25 08:12:21 +0100

    [2023/rust] day25 part_one learning pathfinding

--- 2023/rust/src/day24/mod.rs
+++ 2023/rust/src/day24/mod.rs
@@ -107,6 +107,13 @@ impl Hailstone {
             Intersection::Point((ix, iy))
         }
     }
+    fn pos_at_time(&self, time: i128) -> (i128, i128, i128) {
+        (
+            self.pos.0 + time * self.delta.0,
+            self.pos.1 + time * self.delta.1,
+            self.pos.2 + time * self.delta.2,
+        )
+    }
 }
 
 fn parse_input(input: &str) -> Result<Input> {
@@ -138,10 +145,60 @@ fn solve_one(input: &Input) -> Result<Answer> {
     Ok(Answer::Num(answer as i128))
 }
 
+// each hailstone is a function of time now (I try to ignore non integer collisions here first)
+
+// find line that intersects with all other lines
+
+// (x,y,z) + m *(dx,dy,dz)
+// (a,b,c) + n *(da,db,dc)
+
+// find line that intersects both
+
+// for each two hailstones at time n (a_n, b_n) determine distance vector between (a_n, b_(n+1))
+
 fn solve_two(input: &Input) -> Result<Answer> {
+    let Input { hailstones } = input;
+    for i in 0..hailstones.len() {
+        for j in i + 1..hailstones.len() {
+            // they don't intersect but are there any that are just parallel I got to know
+            let a = &hailstones[i];
+            let b = &hailstones[j];
+            let n0 = a.delta.0 as f64 / b.delta.0 as f64;
+            let n1 = a.delta.1 as f64 / b.delta.1 as f64;
+            let n2 = a.delta.2 as f64 / b.delta.2 as f64;
+            if f64::abs(n0 - n1) < f64::EPSILON && f64::abs(n0 - n2) < f64::EPSILON {
+                println!("found parallel hailstones: {:?}, {:?}", a, b);
+            }
+        }
+    }
+
+    // 2 unknowns => 2 equations
+    // initial unknowns are x,y,z,dx,dy,dz
+    // per line we add n and m but get three equations out
+    // => six lines needed to determine unique solution
+    // A1 + n1*da1 = x + m1*dx
+    // A2 + n2*da2 = x + m2*dx
+    // and so on
+    // A1 + .. + A6 + n1*da1 + .. + n6*da6 = 6x + (m1 + .. + m6)*dx
+    // n1*da1 + .. + n6*da6 - 6x - (m1 + .. + m6)*dx = -(A1 + .. + A6)
+    // with vector [x,y,z,dx,dy,dz,n1,..,n6,m1,..m6]
+    // X = (x,y,z,dx,dy,dz)
+    // (a1,b1,c1) + n1*(da1,db1,dc1) = (x,y,z) + m1*(dx,dy,dz)
+    // a1 + n1*da1 = x + m1*dx => n1 = (x - a1 + m1*dx)/da1
+    // b1 + n1*db1 = y + m1*dy => n1 = (y - b1 + m1*dy)/db1
+    // c1 + n1*dc1 = z + m1*dz => n1 = (z - c1 + m1*dz)/dc1
+    // => m1 
+    // => Intersection at (a1,b1,c1) + n1*(da1,db1,dc1) = A1
+    // (a2,b2,c2) + n2*(da2,db2,dc2) = (x,y,z) + m2*(dx,dy,dz)
+    // => Intersection at (a2,b2,c2) + n2*(da2,db2,dc2) = A2
+    // A1 - A2 = r12*(dx,dy,dz) => 
+    // do the same for A3 - A4 = r34*(dx,dy,dz)
+
+    // no hailstones are parallel good to know :)
+
     let sum = input.xy_crossings_in_area(7.0, 27.0);
     println!("sum = {}", sum);
-    Ok(Answer::Num(-1))
+    Ok(Answer::Num(0))
 }
 
 #[cfg(test)]
---
+++ 2023/rust/src/day25/mod.rs
@@ -0,0 +1,153 @@
+use std::collections::HashSet;
+
+use super::common::*;
+use anyhow::Result;
+use pathfinding::directed::edmonds_karp::*;
+use pathfinding::undirected::connected_components::*;
+
+pub fn part_one(input: &str) -> Result<Answer> {
+    let input = parse_input(input)?;
+    solve_one(&input)
+}
+
+pub fn part_two(input: &str) -> Result<Answer> {
+    let input = parse_input(input)?;
+    solve_two(&input)
+}
+
+struct Input {
+    nodes: Vec<String>,
+    edges: Vec<Edge<usize, i128>>,
+}
+
+fn parse_input(input: &str) -> Result<Input> {
+    let mut node_set = HashSet::new();
+    for line in input.lines() {
+        let (a, b) = line.split_once(":").unwrap();
+        node_set.insert(a.trim().to_string());
+        for bb in b.split_whitespace() {
+            node_set.insert(bb.to_string());
+        }
+    }
+    let mut nodes = Vec::from_iter(node_set.into_iter());
+    nodes.sort();
+    let mut edges = Vec::new();
+    for line in input.lines() {
+        let (a, b) = line.split_once(":").unwrap();
+        let from = nodes.iter().position(|x| x == a.trim()).unwrap();
+        for bb in b.split_whitespace() {
+            let to = nodes.iter().position(|x| x == bb).unwrap();
+            edges.push(((from, to), 1));
+            edges.push(((to, from), 1));
+        }
+    }
+    Ok(Input { nodes, edges })
+}
+
+fn solve_one(input: &Input) -> Result<Answer> {
+    let Input { nodes, edges } = input;
+    let inodes = nodes.iter().enumerate().map(|(i, n)| i).collect::<Vec<_>>();
+    let mut answer = 0;
+    'outer: for s in 0..inodes.len() {
+        for t in s + 1..inodes.len() {
+            let (_, _, min_cut) = edmonds_karp_sparse(&inodes, &s, &t, edges.clone().into_iter());
+            println!(
+                "{}({}) -> {}({}) = {}",
+                nodes[s],
+                s,
+                nodes[t],
+                t,
+                min_cut.len()
+            );
+            if min_cut.len() == 3 {
+                let mut new_edges = edges.clone();
+                println!("{:?}", new_edges.len());
+                let mut starts: Vec<usize> = Vec::new();
+                for ((cut_a, cut_b), _) in min_cut.iter() {
+                    starts.push(*cut_a);
+                    starts.push(*cut_b);
+                    println!(
+                        "removing {}({}), {}({})",
+                        nodes[*cut_a], *cut_a, nodes[*cut_b], *cut_b
+                    );
+                    new_edges = new_edges
+                        .into_iter()
+                        .filter(|((a, b), c)| {
+                            (a != cut_a || b != cut_b) && (a != cut_b || b != cut_a)
+                        })
+                        .collect();
+                }
+                println!("{:?}", new_edges.len());
+
+                let components = connected_components(&inodes, |n| {
+                    let mut neighs: Vec<usize> = new_edges
+                        .iter()
+                        .filter(|((a, _), _)| *a == *n)
+                        .map(|((_, b), _)| *b)
+                        .collect();
+                    neighs
+                });
+                for com in components.iter() {
+                    println!("com count {}", com.len());
+                    for k in com.iter() {
+                        print!("{}({}), ", nodes[*k], k);
+                    }
+                    println!();
+                    println!("com is {:?}", com);
+                }
+                assert_eq!(components.len(), 2);
+                answer = components[0].len() * components[1].len();
+                break 'outer;
+            }
+        }
+    }
+    Ok(Answer::Num(answer as i128))
+}
+
+fn solve_two(input: &Input) -> Result<Answer> {
+    todo!() // solve other tasks...
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::sync::LazyLock;
+    use test::Bencher;
+
+    static TEST: LazyLock<String> = local_file!("test.txt");
+    static INPUT: LazyLock<String> = local_file!("input.txt");
+
+    #[test]
+    fn test_one() -> Result<()> {
+        let answer = super::part_one(&TEST)?;
+        assert_eq!(answer, Answer::Num(54));
+        Ok(())
+    }
+    #[test]
+    fn part_one() -> Result<()> {
+        let answer = super::part_one(&INPUT)?;
+        assert_eq!(answer, Answer::Num(-1));
+        Ok(())
+    }
+    #[test]
+    fn test_two() -> Result<()> {
+        let answer = super::part_two(&TEST)?;
+        assert_eq!(answer, Answer::Num(-1));
+        Ok(())
+    }
+    #[test]
+    fn part_two() -> Result<()> {
+        let answer = super::part_two(&INPUT)?;
+        assert_eq!(answer, Answer::Num(-1));
+        Ok(())
+    }
+
+    #[bench]
+    fn bench_part_one(b: &mut Bencher) {
+        b.iter(|| part_one())
+    }
+    #[bench]
+    fn bench_part_two(b: &mut Bencher) {
+        b.iter(|| part_two())
+    }
+}
