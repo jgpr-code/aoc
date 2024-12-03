89fd5ca366e069fe2b35ca41350d659db3c4fcc1
Jörg Planner <joerg.planner@ul.com>
2024-12-03 10:02:45 +0100

    [2024/rust] day03 part2

--- 2024/rust/day03/src/main.rs
+++ 2024/rust/day03/src/main.rs
@@ -1,7 +1,7 @@
 #![feature(test)]
 extern crate test;
 
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use common::regx;
 use common::Answer;
 use std::io;
@@ -24,29 +24,58 @@ pub fn part_two(input: &str) -> Result<Answer> {
 }
 
 struct Input {
-    lines: Vec<String>,
+    haystack: String,
 }
 
 fn parse_input(input: &str) -> Result<Input> {
-    let lines = input.lines().map(|l| String::from(l)).collect();
-    Ok(Input { lines })
+    Ok(Input {
+        haystack: String::from(input),
+    })
 }
 
 fn solve_one(input: &Input) -> Result<Answer> {
-    let Input { lines } = input;
+    let Input { haystack } = input;
     let mul_re = regx!(r"mul\((\d{1,3}),(\d{1,3})\)");
     let mut sum = 0;
-    for line in lines {
-        for (_, [a, b]) in mul_re.captures_iter(line).map(|c| c.extract()) {
-            sum += i128::from_str_radix(a, 10)? * i128::from_str_radix(b, 10)?;
-        }
+    for (_, [a, b]) in mul_re.captures_iter(haystack).map(|c| c.extract()) {
+        sum += i128::from_str_radix(a, 10)? * i128::from_str_radix(b, 10)?;
     }
     Ok(Answer::Num(sum))
 }
 
 fn solve_two(input: &Input) -> Result<Answer> {
-    let _unused = input;
-    Ok(Answer::Num(0))
+    let Input { haystack } = input;
+    let mul_or_do_re = regx!(r"mul\((\d{1,3}),(\d{1,3})\)|(do|don't)\(\)");
+    let mut do_mul = true;
+    let mut sum = 0;
+    for caps in mul_or_do_re.captures_iter(haystack) {
+        if let Some(a) = caps.get(1) {
+            if do_mul {
+                let b = caps
+                    .get(2)
+                    .ok_or(anyhow!("second number missing from mul"))?;
+                let astr: &str = a.into();
+                let bstr: &str = b.into();
+                // println!("{}, {}", astr, bstr);
+                sum += i128::from_str_radix(astr, 10)? * i128::from_str_radix(bstr, 10)?;
+            }
+        } else {
+            let what_do = caps
+                .get(3)
+                .map_or(Err(anyhow!("match should be do or don't")), |c| {
+                    Ok(c.as_str())
+                })?;
+            // println!("{}", what_do);
+            if what_do == "do" {
+                do_mul = true;
+            } else if what_do == "don't" {
+                do_mul = false;
+            } else {
+                return Err(anyhow!("neither do nor don't"));
+            }
+        }
+    }
+    Ok(Answer::Num(sum))
 }
 
 // Quickly obtain answers by running
@@ -60,6 +89,7 @@ mod day03_tests {
     use test::Bencher;
 
     static TEST: LazyLock<String> = local_file!("test");
+    static TEST2: LazyLock<String> = local_file!("test2");
     static INPUT: LazyLock<String> = local_file!("input");
 
     #[test]
@@ -79,13 +109,13 @@ mod day03_tests {
     }
     #[test]
     fn test_two() -> Result<()> {
-        let answer = super::part_two(&TEST)?;
-        assert_eq!(answer, Answer::Num(0));
+        let answer = super::part_two(&TEST2)?;
+        assert_eq!(answer, Answer::Num(48));
         Ok(())
     }
     fn part_two_impl() -> Result<()> {
         let answer = super::part_two(&INPUT)?;
-        assert_eq!(answer, Answer::Num(0));
+        assert_eq!(answer, Answer::Num(88802350));
         Ok(())
     }
     #[bench]
