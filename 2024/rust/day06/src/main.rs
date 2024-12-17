33d792dd344f9ea31407ccb4a485940f5e3a9020
Jörg Planner <j.planner@gmx.de>
2024-12-17 10:23:31 +0100

    [2024/rust] day06 part2 no blocking allowed

--- 2024/rust/day06/src/main.rs
+++ 2024/rust/day06/src/main.rs
@@ -3,7 +3,10 @@ extern crate test;
 
 use anyhow::{anyhow, Result};
 use common::Answer;
-use std::{collections::HashMap, io};
+use std::{
+    collections::{HashMap, HashSet},
+    io,
+};
 
 pub fn main() -> Result<()> {
     let stdin = io::read_to_string(io::stdin())?;
@@ -33,50 +36,8 @@ fn next_dir(dir: usize) -> usize {
 struct Input {
     grid: Vec<Vec<char>>,
     guard_start: (usize, usize),
-    row_obstacles: HashMap<usize, Vec<usize>>,
-    col_obstacles: HashMap<usize, Vec<usize>>,
-}
-
-struct Line {
-    start: (usize, usize),
-    end: (usize, usize),
-    dir: usize,
-}
-
-impl Line {
-    fn intersect(&self, other: &Line) -> Option<(usize, usize)> {
-        if next_dir(self.dir) != other.dir {
-            return None;
-        }
-        // spos, sfrom, sto  opos, oto
-
-        // opos between sfrom and sto
-        match self.dir {
-            0 => {
-                // UP
-                if o_end_col < s_start_col {
-                    return None;
-                }
-                if o_end_row < s_end_row && o_end_row >= s_start_row {
-                    return Some(s_end_row - 1);
-                } else {
-                    return None;
-                }
-            }
-            1 => {
-                // RIGHT
-            }
-            2 => {
-                // DOWN
-            }
-            3 => {
-                // LEFT
-            }
-            _ => panic!("must never happen"),
-        }
-
-        Some((0, 0))
-    }
+    row_obstacles: HashMap<usize, Vec<usize>>, // obstacles cols arranged by row
+    col_obstacles: HashMap<usize, Vec<usize>>, // obstacles rows arranged by col
 }
 
 struct WalkingGuard {
@@ -219,6 +180,119 @@ fn solve_one(input: &Input) -> Result<Answer> {
     Ok(Answer::Num(walking_guard.count_walked() as i128))
 }
 
+struct Line {
+    start: (usize, usize),
+    end: (usize, usize),
+    dir: usize,
+}
+
+impl Line {
+    fn new(start: (usize, usize), end: (usize, usize), dir: usize) -> Self {
+        Self { start, end, dir }
+    }
+    fn intersect(
+        &self,
+        other: &Line,
+        row_obstacles: &HashMap<usize, Vec<usize>>,
+        col_obstacles: &HashMap<usize, Vec<usize>>,
+    ) -> Option<(usize, usize)> {
+        if next_dir(self.dir) != other.dir {
+            return None;
+        }
+        let (s_row, s_col) = self.start;
+        let (e_row, e_col) = self.end;
+        let (os_row, os_col) = other.start;
+        let (oe_row, oe_col) = other.end;
+        match self.dir {
+            0 => {
+                // UP -> RIGHT
+                assert!(os_row == oe_row);
+                assert!(s_col == e_col);
+                assert!(e_row < s_row);
+                if e_row <= os_row && os_row <= s_row && s_col < oe_col {
+                    let intersection = Some((os_row, s_col));
+                    let row_obstacles = row_obstacles
+                        .get(&os_row)
+                        .expect("there should be a row obstacle");
+                    let partition = row_obstacles.partition_point(|&col| col <= s_col);
+                    if row_obstacles[partition] == oe_col + 1 {
+                        return intersection;
+                    } else {
+                        return None;
+                    }
+                } else {
+                    return None;
+                }
+            }
+            1 => {
+                // RIGHT -> DOWN
+                assert!(os_col == oe_col);
+                assert!(s_row == e_row);
+                assert!(s_col < e_col);
+                if s_col <= os_col && os_col <= e_col && s_row < oe_row {
+                    let intersection = Some((s_row, os_col));
+                    let col_obstacles = col_obstacles
+                        .get(&os_col)
+                        .expect("there should be a col obstacle");
+                    let partition = col_obstacles.partition_point(|&row| row <= s_row);
+                    if col_obstacles[partition] == oe_row + 1 {
+                        return intersection;
+                    } else {
+                        return None;
+                    }
+                } else {
+                    return None;
+                }
+            }
+            2 => {
+                // DOWN -> LEFT
+                assert!(os_row == oe_row);
+                assert!(s_col == e_col);
+                assert!(s_row < e_row);
+                if s_row <= os_row && os_row <= e_row && oe_col < s_col {
+                    let intersection = Some((os_row, s_col));
+                    let row_obstacles = row_obstacles
+                        .get(&os_row)
+                        .expect("there should be a row obstacle");
+                    let partition = row_obstacles.partition_point(|&col| col <= s_col);
+                    if row_obstacles[partition - 1] == oe_col - 1 {
+                        return intersection;
+                    } else {
+                        return None;
+                    }
+                } else {
+                    return None;
+                }
+            }
+            3 => {
+                // LEFT -> UP
+                assert!(os_col == oe_col);
+                assert!(s_row == e_row);
+                assert!(e_col < s_col);
+                if e_col <= os_col && os_col <= s_col && oe_row < s_row {
+                    let intersection = Some((s_row, os_col));
+                    let col_obstacles = col_obstacles
+                        .get(&os_col)
+                        .expect("there should be a col obstacle");
+                    let partition = col_obstacles.partition_point(|&row| row <= s_row);
+                    if col_obstacles[partition - 1] == oe_row - 1 {
+                        return intersection;
+                    } else {
+                        return None;
+                    }
+                } else {
+                    return None;
+                }
+            }
+            _ => panic!("not a direction"),
+        }
+    }
+}
+
+fn inside(row: i128, col: i128, rows: i128, cols: i128) -> bool {
+    0 <= row && row < rows && 0 <= col && col < cols
+}
+
 fn solve_two(input: &Input) -> Result<Answer> {
     // idea is:
     // for current line:
@@ -232,40 +306,225 @@ fn solve_two(input: &Input) -> Result<Answer> {
         col_obstacles,
     } = input;
 
+    let mut grid_copy = grid.clone();
+
     let rows = grid.len();
     let cols = grid[0].len();
-    let mut guard_pos = *guard_start;
+    let mut row_guard = guard_start.0;
+    let mut col_guard = guard_start.1;
     let mut guard_dir = 0;
-    // let mut existing_lines = Vec::new();
+    let mut existing_lines = Vec::new();
+    let mut count_positions = 0;
+    let mut count_loops = 0;
+    let mut loop_obstacles = HashSet::new();
     loop {
+        let mut break_loop = false;
+        let (above, below) = vertical_obstacles(row_guard, col_guard, col_obstacles);
+        let (left, right) = horizontal_obstacles(row_guard, col_guard, row_obstacles);
         match guard_dir {
             0 => {
                 // UP
-                if let Some(col_obstacle) = col_obstacles.get(&guard_pos.1) {
-                    let first_below = col_obstacle.partition_point(|&row| row < guard_pos.0);
+                let new_row = if let Some(obstacle_row) = above {
+                    obstacle_row + 1
+                } else {
+                    break_loop = true;
+                    0
+                };
+                let walked_line =
+                    Line::new((row_guard, col_guard), (new_row, col_guard), guard_dir);
+                for other_line in existing_lines.iter() {
+                    if let Some((irow, icol)) =
+                        walked_line.intersect(other_line, row_obstacles, col_obstacles)
+                    {
+                        let oirow = irow as i128 + DROW[guard_dir];
+                        let oicol = icol as i128 + DCOL[guard_dir];
+                        if inside(oirow, oicol, rows as i128, cols as i128)
+                            && grid_copy[oirow as usize][oicol as usize] == '.'
+                        {
+                            count_loops += 1;
+                            loop_obstacles.insert((oirow, oicol));
+                        }
+                    }
                 }
+                for row in new_row..=row_guard {
+                    if grid_copy[row][col_guard] != 'X' {
+                        grid_copy[row][col_guard] = 'X';
+                        count_positions += 1;
+                    }
+                }
+                row_guard = new_row;
+                guard_dir = next_dir(guard_dir);
+                existing_lines.push(walked_line);
             }
             1 => {
                 // RIGHT
-                if let Some(row_obstacle) = row_obstacles.get(&guard_pos.0) {
-                    let first_right = row_obstacle.partition_point(|&col| col < guard_pos.1);
+                let new_col = if let Some(obstacle_col) = right {
+                    obstacle_col - 1
+                } else {
+                    break_loop = true;
+                    cols - 1
+                };
+                let walked_line =
+                    Line::new((row_guard, col_guard), (row_guard, new_col), guard_dir);
+                for other_line in existing_lines.iter() {
+                    if let Some((irow, icol)) =
+                        walked_line.intersect(other_line, row_obstacles, col_obstacles)
+                    {
+                        let oirow = irow as i128 + DROW[guard_dir];
+                        let oicol = icol as i128 + DCOL[guard_dir];
+                        if inside(oirow, oicol, rows as i128, cols as i128)
+                            && grid_copy[oirow as usize][oicol as usize] == '.'
+                        {
+                            count_loops += 1;
+                            loop_obstacles.insert((oirow, oicol));
+                        }
+                    }
                 }
+                for col in col_guard..=new_col {
+                    if grid_copy[row_guard][col] != 'X' {
+                        grid_copy[row_guard][col] = 'X';
+                        count_positions += 1;
+                    }
+                }
+                col_guard = new_col;
+                guard_dir = next_dir(guard_dir);
+                existing_lines.push(walked_line);
             }
             2 => {
                 // DOWN
-                if let Some(col_obstacle) = col_obstacles.get(&guard_pos.1) {
-                    let first_below = col_obstacle.partition_point(|&row| row < guard_pos.0);
+                let new_row = if let Some(obstacle_row) = below {
+                    obstacle_row - 1
+                } else {
+                    break_loop = true;
+                    rows - 1
+                };
+                let walked_line =
+                    Line::new((row_guard, col_guard), (new_row, col_guard), guard_dir);
+                for other_line in existing_lines.iter() {
+                    if let Some((irow, icol)) =
+                        walked_line.intersect(other_line, row_obstacles, col_obstacles)
+                    {
+                        let oirow = irow as i128 + DROW[guard_dir];
+                        let oicol = icol as i128 + DCOL[guard_dir];
+                        if inside(oirow, oicol, rows as i128, cols as i128)
+                            && grid_copy[oirow as usize][oicol as usize] == '.'
+                        {
+                            count_loops += 1;
+                            loop_obstacles.insert((oirow, oicol));
+                        }
+                    }
                 }
+                for row in row_guard..=new_row {
+                    if grid_copy[row][col_guard] != 'X' {
+                        grid_copy[row][col_guard] = 'X';
+                        count_positions += 1;
+                    }
+                }
+                row_guard = new_row;
+                guard_dir = next_dir(guard_dir);
+                existing_lines.push(walked_line);
             }
             3 => {
                 // LEFT
-                if let Some(row_obstacle) = row_obstacles.get(&guard_pos.0) {
-                    let first_right = row_obstacle.partition_point(|&col| col < guard_pos.1);
+                let new_col = if let Some(obstacle_col) = left {
+                    obstacle_col + 1
+                } else {
+                    break_loop = true;
+                    0
+                };
+                let walked_line =
+                    Line::new((row_guard, col_guard), (row_guard, new_col), guard_dir);
+                for other_line in existing_lines.iter() {
+                    if let Some((irow, icol)) =
+                        walked_line.intersect(other_line, row_obstacles, col_obstacles)
+                    {
+                        let oirow = irow as i128 + DROW[guard_dir];
+                        let oicol = icol as i128 + DCOL[guard_dir];
+                        if inside(oirow, oicol, rows as i128, cols as i128)
+                            && grid_copy[oirow as usize][oicol as usize] == '.'
+                        {
+                            count_loops += 1;
+                            loop_obstacles.insert((oirow, oicol));
+                        }
+                    }
                 }
+                for col in new_col..=col_guard {
+                    if grid_copy[row_guard][col] != 'X' {
+                        grid_copy[row_guard][col] = 'X';
+                        count_positions += 1;
+                    }
+                }
+                col_guard = new_col;
+                guard_dir = next_dir(guard_dir);
+                existing_lines.push(walked_line);
+            }
+            _ => {
+                panic!("not a direction!")
             }
         }
+        if break_loop {
+            break;
+        }
     }
-    Ok(Answer::Num(0))
+    println!("count positions: {}", count_positions);
+    println!("count loops: {}", count_loops);
+    for row in 0..rows {
+        for col in 0..cols {
+            if loop_obstacles.contains(&(row as i128, col as i128)) {
+                print!("O");
+            } else {
+                print!("{}", grid_copy[row][col]);
+            }
+        }
+        println!();
+    }
+    Ok(Answer::Num(count_loops))
+}
+
+fn vertical_obstacles(
+    row_guard: usize,
+    col_guard: usize,
+    col_obstacles: &HashMap<usize, Vec<usize>>,
+) -> (Option<usize>, Option<usize>) {
+    if let Some(col_obstacles) = col_obstacles.get(&col_guard) {
+        let hit_below = col_obstacles.partition_point(|&row| row < row_guard);
+        let above = if hit_below > 0 {
+            Some(col_obstacles[hit_below - 1])
+        } else {
+            None
+        };
+        let below = if hit_below < col_obstacles.len() {
+            Some(col_obstacles[hit_below])
+        } else {
+            None
+        };
+        (above, below)
+    } else {
+        (None, None)
+    }
+}
+
+fn horizontal_obstacles(
+    row_guard: usize,
+    col_guard: usize,
+    row_obstacles: &HashMap<usize, Vec<usize>>,
+) -> (Option<usize>, Option<usize>) {
+    if let Some(row_obstacles) = row_obstacles.get(&row_guard) {
+        let hit_right = row_obstacles.partition_point(|&col| col < col_guard);
+        let left = if hit_right > 0 {
+            Some(row_obstacles[hit_right - 1])
+        } else {
+            None
+        };
+        let right = if hit_right < row_obstacles.len() {
+            Some(row_obstacles[hit_right])
+        } else {
+            None
+        };
+        (left, right)
+    } else {
+        (None, None)
+    }
 }
 
 // Quickly obtain answers by running
@@ -300,7 +559,7 @@ mod day06_tests {
     #[test]
     fn test_two() -> Result<()> {
         let answer = super::part_two(&TEST)?;
-        assert_eq!(answer, Answer::Num(6));
+        assert_eq!(answer, Answer::Num(7)); // 6
         Ok(())
     }
     fn part_two_impl() -> Result<()> {
