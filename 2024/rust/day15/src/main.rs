569238879b136655772579a230cbdc32f2cf39e3
Jörg Planner <j.planner@gmx.de>
2024-12-21 19:09:27 +0100

    [2024/rust] day15 part1 improved

--- 2024/rust/day15/src/main.rs
+++ 2024/rust/day15/src/main.rs
@@ -24,19 +24,13 @@ pub fn part_two(input: &str) -> Result<Answer> {
 
 #[derive(Clone)]
 struct Input {
-    field: Vec<Vec<char>>,
-    // rows: usize,
-    // cols: usize,
+    robot: (i32, i32),
+    boxes: HashSet<(i32, i32)>,
+    obstructions: HashSet<(i32, i32)>,
     instructions: Vec<char>,
-    boxes: HashSet<(usize, usize)>,
-    robot: (usize, usize),
 }
 
 impl Input {
-    // input is constructed s.t. this is not necessary (i.e. surrounded by border '#')
-    // fn inside(&self, row: i32, col: i32) -> bool {
-    //     0 <= row && row < self.rows as i32 && 0 <= col && col < self.cols as i32
-    // }
     fn simulate_instructions(&mut self) -> Result<()> {
         let instruction = self.instructions.clone();
         for instruction in instruction.into_iter() {
@@ -44,58 +38,64 @@ impl Input {
         }
         Ok(())
     }
+    fn move_box(&self, a_box: (i32, i32), dir: (i32, i32)) -> Option<Vec<(i32, i32)>> {
+        let move_to = (a_box.0 + dir.0, a_box.1 + dir.1);
+        if self.obstructions.contains(&move_to) {
+            return None;
+        }
+        if self.boxes.contains(&move_to) {
+            if let Some(mut moved_boxes) = self.move_box(move_to, dir) {
+                moved_boxes.push(a_box);
+                return Some(moved_boxes);
+            } else {
+                return None;
+            }
+        }
+        Some(vec![a_box])
+    }
     fn simulate_instruction(&mut self, instruction: char) -> Result<()> {
-        let (drow, dcol) = match instruction {
+        let dir = match instruction {
             '^' => (-1, 0),
             '>' => (0, 1),
             'v' => (1, 0),
             '<' => (0, -1),
             _ => return Err(anyhow!("invalid instruction {}", instruction)),
         };
-        let nrow = self.robot.0 as i32 + drow;
-        let ncol = self.robot.1 as i32 + dcol;
-        let target = self.field[nrow as usize][ncol as usize];
-        match target {
-            '#' => {}
-            '.' => {
-                self.field[nrow as usize][ncol as usize] = '@';
-                self.field[self.robot.0][self.robot.1] = '.';
-                self.robot = (nrow as usize, ncol as usize);
-            }
-            'O' => {
-                let mut mrow = nrow;
-                let mut mcol = ncol;
-                while self.field[mrow as usize][mcol as usize] == 'O' {
-                    mrow += drow;
-                    mcol += dcol;
+        let move_to = (self.robot.0 + dir.0, self.robot.1 + dir.1);
+        // let nrow = self.robot.0 as i32 + drow;
+        // let ncol = self.robot.1 as i32 + dcol;
+        // let target = self.field[nrow as usize][ncol as usize];
+        if self.obstructions.contains(&move_to) {
+            return Ok(());
+        }
+        if self.boxes.contains(&move_to) {
+            if let Some(moved_boxes) = self.move_box(move_to, dir) {
+                self.robot = move_to;
+                for moved_box in moved_boxes.iter() {
+                    self.boxes.remove(&moved_box);
                 }
-                match self.field[mrow as usize][mcol as usize] {
-                    '#' => {}
-                    '.' => {
-                        self.field[mrow as usize][mcol as usize] = 'O';
-                        self.field[nrow as usize][ncol as usize] = '@';
-                        self.field[self.robot.0][self.robot.1] = '.';
-                        self.robot = (nrow as usize, ncol as usize);
-                        self.boxes.remove(&(nrow as usize, ncol as usize));
-                        self.boxes.insert((mrow as usize, mcol as usize));
-                    }
-                    x => return Err(anyhow!("movement to weird field {}", x)),
+                for moved_box in moved_boxes.iter() {
+                    let moved_box = (moved_box.0 + dir.0, moved_box.1 + dir.1);
+                    self.boxes.insert(moved_box);
                 }
             }
-            _ => return Err(anyhow!("invalid field entry {}", target)),
+        } else {
+            self.robot = move_to;
         }
         Ok(())
     }
-    fn gps_coordinate(a_box: &(usize, usize)) -> usize {
-        a_box.0 * 100 + a_box.1
+    fn gps_coordinate(a_box: &(i32, i32)) -> i128 {
+        assert!(a_box.0 > 0 && a_box.1 > 0);
+        (a_box.0 * 100 + a_box.1) as i128
     }
-    fn gps_sum(&self) -> usize {
+    fn gps_sum(&self) -> i128 {
         let mut sum = 0;
         for a_box in self.boxes.iter() {
             sum += Self::gps_coordinate(a_box);
         }
         sum
     }
+    fn enlarge(&mut self) {}
 }
 
 fn parse_input(input: &str) -> Result<Input> {
@@ -103,39 +103,36 @@ fn parse_input(input: &str) -> Result<Input> {
         .trim()
         .split_once("\n\n")
         .expect("splitting input should work");
-    let mut field = Vec::new();
     let mut boxes = HashSet::new();
+    let mut obstructions = HashSet::new();
     let mut robot = None;
     for (row, line) in field_str.lines().enumerate() {
-        let mut line_chars = Vec::new();
         for (col, c) in line.chars().enumerate() {
-            line_chars.push(c);
+            let (row, col) = (row as i32, col as i32);
             if c == '@' {
                 robot = Some((row, col));
             }
             if c == 'O' {
                 boxes.insert((row, col));
             }
+            if c == '#' {
+                obstructions.insert((row, col));
+            }
         }
-        field.push(line_chars);
     }
-    // let rows = field.len();
-    // let cols = field.get(0).expect("at least one line is expected").len();
     let instructions = instructions_str.lines().flat_map(|l| l.chars()).collect();
     Ok(Input {
-        field,
-        // rows,
-        // cols,
-        instructions,
-        boxes,
         robot: robot.ok_or(anyhow!("robot must be present"))?,
+        boxes,
+        obstructions,
+        instructions,
     })
 }
 
 fn solve_one(input: &Input) -> Result<Answer> {
     let mut input = input.clone();
     input.simulate_instructions()?;
-    Ok(Answer::Num(input.gps_sum() as i128))
+    Ok(Answer::Num(input.gps_sum()))
 }
 
 fn solve_two(input: &Input) -> Result<Answer> {
@@ -175,7 +172,7 @@ mod day15_tests {
     #[test]
     fn test_two() -> Result<()> {
         let answer = super::part_two(&TEST)?;
-        assert_eq!(answer, Answer::Num(0));
+        assert_eq!(answer, Answer::Num(9021));
         Ok(())
     }
     fn part_two_impl() -> Result<()> {
