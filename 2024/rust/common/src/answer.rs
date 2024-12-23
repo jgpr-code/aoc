2de945dd4cf4266be699c07ecb113de508027070
Jörg Planner <j.planner@gmx.de>
2024-12-23 10:02:28 +0100

    [2024/rust] add conversion utilities to answer type

--- 2024/rust/common/src/answer.rs
+++ 2024/rust/common/src/answer.rs
@@ -5,12 +5,21 @@ pub enum Answer {
     Num(i128),
     Str(String),
 }
-
 impl Display for Answer {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match &self {
             Answer::Num(i) => write!(f, "{}", i),
             Answer::Str(s) => write!(f, "{}", s),
         }
+    }
+}
+impl From<&str> for Answer {
+    fn from(value: &str) -> Self {
+        Self::Str(String::from(value))
+    }
+}
+impl From<i128> for Answer {
+    fn from(value: i128) -> Self {
+        Self::Num(value)
     }
 }
