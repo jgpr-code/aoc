3d7ff832450353dead36cd371eebcc59212cae07
Jörg Planner <j.planner@gmx.de>
2024-12-28 13:12:19 +0100

    Revert "[2024/rust] day21 SEE THE STUPIDEST BUG EVER"
    This reverts commit 472b690edf489018ddba2f1a100cb4544c34f7fd.

--- 2024/rust/day21/src/main.rs
+++ 2024/rust/day21/src/main.rs
@@ -7,7 +7,7 @@ use common::{
     Answer,
 };
 use std::{
-    collections::{HashMap, HashSet, VecDeque},
+    collections::{HashMap, HashSet},
     io,
     iter::repeat,
 };
@@ -48,80 +48,87 @@ enum KeypadType {
 }
 struct Keypad {
     keys: HashMap<char, Point>,
+    position: Point,
     keypad_type: KeypadType,
-    cached_move_sequences: HashMap<(char, char), HashSet<String>>,
-    cached_word_sequences: HashMap<(char, String), HashSet<String>>,
 }
 impl Keypad {
-    fn word_sequences(&mut self, start: char, word: &str) -> HashSet<String> {
+    fn shortest_word_sequences(&self, start: char, word: &str) -> HashSet<String> {
         if word.len() == 0 {
             return HashSet::from(["".to_string()]);
         }
-        if let Some(possibilities) = self.cached_word_sequences.get(&(start, word.to_string())) {
-            return possibilities.clone();
-        }
         let to = word.chars().nth(0).unwrap();
         let mut possibilities = HashSet::new();
-        let paths = self.move_sequences(start, to);
-        let remaining_possibilities = self.word_sequences(to, &word[1..]);
-        if remaining_possibilities.len() == 0 {
-            panic!("no possibilities");
-        }
+        let paths = self.shortest_move_sequences(start, to);
+        let remaining_possibilities = self.shortest_word_sequences(to, &word[1..]);
         for path in paths {
             for remaining_possibility in remaining_possibilities.iter() {
                 possibilities.insert(format!("{}A{}", path, remaining_possibility));
             }
         }
-        self.cached_word_sequences
-            .insert((start, word.to_string()), possibilities.clone());
         possibilities
     }
-    fn move_sequences(&mut self, from: char, to: char) -> HashSet<String> {
-        // must simulate all paths :(
+    fn shortest_move_sequences(&self, from: char, to: char) -> HashSet<String> {
+        // insight: just picking one sequence is wrong (trial and error approach)
+        // for example: directional keypad is at '<' and numeric keypad is at '5'
+        // to go from '5' to '1' we would use either '<v' or 'v<', but because the
+        // directional keypad starts at '<' the sequence '<v' is better!
+        // but does this really matter (seems like it doesn't OR IT DOES):
+        // e.g. typing 53 requires <^^A|^^<A >vA|v>A
+        // >vA requires vA<A>^A|vA<A^>A
+        // v>A requires v<A>A^A
+
+        // assumption: extra turns should be avoided, because they get expensive on the next keypad => probably this is wrong also
         if from == to {
             return HashSet::from(["".to_string()]);
         }
-        if let Some(possibilities) = self.cached_move_sequences.get(&(from, to)) {
-            return possibilities.clone();
-        }
-        let bound = match self.keypad_type {
-            KeypadType::Directional => point!(3, 2),
-            KeypadType::Numeric => point!(3, 4),
-        };
-        let invalid = match self.keypad_type {
-            KeypadType::Directional => point!(0, 0),
-            KeypadType::Numeric => point!(0, 3),
-        };
         let from_position = self.keys[&from];
         let to_position = self.keys[&to];
         let delta = to_position - from_position;
-        let length_cap = (i128::abs(delta.x) + i128::abs(delta.y)) as usize;
+        let cx = if delta.x >= 0 { '>' } else { '<' };
+        let nx = i128::abs(delta.x) as usize;
+        let sx = cx.to_string().repeat(nx);
+        let cy = if delta.y >= 0 { 'v' } else { '^' };
+        let ny = i128::abs(delta.y) as usize;
+        let sy = cy.to_string().repeat(ny);
         let mut possibilities = HashSet::new();
-        let mut queue = VecDeque::new();
-        queue.push_back((from_position, String::from("")));
-        while let Some((position, accu)) = queue.pop_front() {
-            if position == to_position {
-                possibilities.insert(accu);
-                continue;
-            }
-            if accu.len() < length_cap {
-                for neigh_delta in point::NEIGH4 {
-                    let neigh = position + neigh_delta;
-                    if neigh.inside_point_bound(&bound) && neigh != invalid {
-                        let new_accu = match neigh_delta {
-                            point::UP => format!("{accu}^"),
-                            point::RIGHT => format!("{accu}>"),
-                            point::DOWN => format!("{accu}v"),
-                            point::LEFT => format!("{accu}<"),
-                            _ => panic!("neigh must be in NEIGH4"),
-                        };
-                        queue.push_back((neigh, new_accu));
-                    }
+        match self.keypad_type {
+            // 789
+            // 456
+            // 123
+            //  0A
+            KeypadType::Numeric => match from {
+                '7' | '4' | '1' if to == '0' || to == 'A' => {
+                    // change x then y
+                    possibilities.insert(format!("{sx}{sy}"));
                 }
-            }
+                '0' | 'A' if to == '7' || to == '4' || to == '1' => {
+                    // change y then x
+                    possibilities.insert(format!("{sy}{sx}"));
+                }
+                _ => {
+                    // either works
+                    possibilities.insert(format!("{sx}{sy}"));
+                    possibilities.insert(format!("{sy}{sx}"));
+                }
+            },
+            //  ^A
+            // <v>
+            KeypadType::Directional => match from {
+                '<' => {
+                    // change x then y
+                    possibilities.insert(format!("{sx}{sy}"));
+                }
+                '^' | 'A' if to == '<' => {
+                    // change y then x
+                    possibilities.insert(format!("{sy}{sx}"));
+                }
+                _ => {
+                    // either works
+                    possibilities.insert(format!("{sx}{sy}"));
+                    possibilities.insert(format!("{sy}{sx}"));
+                }
+            },
         }
-        self.cached_move_sequences
-            .insert((from, to), possibilities.clone());
         possibilities
     }
     // +---+---+---+
@@ -141,14 +148,12 @@ impl Keypad {
             ('1', point!(0, 2)), ('2', point!(1, 2)), ('3', point!(2, 2)),
                                  ('0', point!(1, 3)), ('A', point!(2, 3)),
         ]);
+        let position = keys[&'A'];
         let keypad_type = KeypadType::Numeric;
-        let cached_move_sequences = HashMap::new();
-        let cached_word_sequences = HashMap::new();
         Self {
             keys,
+            position,
             keypad_type,
-            cached_move_sequences,
-            cached_word_sequences,
         }
     }
     //     +---+---+
@@ -162,14 +167,12 @@ impl Keypad {
                                  ('^', point!(1, 0)), ('A', point!(2, 0)),
             ('<', point!(0, 1)), ('v', point!(1, 1)), ('>', point!(2, 1)),
         ]);
+        let position = keys[&'A'];
         let keypad_type = KeypadType::Directional;
-        let cached_move_sequences = HashMap::new();
-        let cached_word_sequences = HashMap::new();
         Self {
             keys,
+            position,
             keypad_type,
-            cached_move_sequences,
-            cached_word_sequences,
         }
     }
 }
@@ -180,16 +183,16 @@ fn code_value(code: &str) -> i128 {
 
 fn solve_one(input: &Input) -> Result<Answer> {
     let Input { codes } = input;
-    let mut numeric_pad = Keypad::new_numeric();
-    let mut directional_pad = Keypad::new_directional();
+    let numeric_pad = Keypad::new_numeric();
+    let directional_pad = Keypad::new_directional();
     let mut sum = 0;
     for code in codes {
         let mut minimal_length = usize::MAX;
-        let typed_by_first = numeric_pad.word_sequences('A', code);
+        let typed_by_first = numeric_pad.shortest_word_sequences('A', code);
         for tf in typed_by_first.iter() {
-            let typed_by_second = directional_pad.word_sequences('A', tf);
+            let typed_by_second = directional_pad.shortest_word_sequences('A', tf);
             for ts in typed_by_second.iter() {
-                let typed_by_me = directional_pad.word_sequences('A', ts);
+                let typed_by_me = directional_pad.shortest_word_sequences('A', ts);
                 for tm in typed_by_me.iter() {
                     minimal_length = usize::min(minimal_length, tm.len());
                 }
@@ -200,7 +203,7 @@ fn solve_one(input: &Input) -> Result<Answer> {
     }
     // first try by not looking at all possibilities => 138560 => wrong (too high)
     // second answer 137420 => wrong (too high) ?
-    // third answer 134121 => wrong (still too high) (OH GOD SEE THE + 1 IN THE LINE BELOW!)
+    // third answer 134121 => wrong (still too high)
     Ok(Answer::Num(sum + 1))
 }
 
