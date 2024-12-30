1dff4930a4131ee23cbc3aee028979defdc310e3
Jörg Planner <j.planner@gmx.de>
2024-12-30 15:34:04 +0100

    [2024/rust] day22 part2

--- 2024/rust/day22/src/main.rs
+++ 2024/rust/day22/src/main.rs
@@ -1,9 +1,13 @@
 #![feature(test)]
+#![feature(iter_map_windows)]
 extern crate test;
 
 use anyhow::Result;
 use common::Answer;
-use std::io;
+use std::{
+    collections::{HashMap, HashSet},
+    io,
+};
 
 pub fn main() -> Result<()> {
     let stdin = io::read_to_string(io::stdin())?;
@@ -35,11 +39,13 @@ fn parse_input(input: &str) -> Result<Input> {
         .collect::<Result<Vec<_>, _>>()?;
     Ok(Input { secret_nums })
 }
+
 fn mix_prune(num: &mut i128, mix: i128) {
     const PRUNE: i128 = 16777216;
     *num ^= mix;
     *num %= PRUNE;
 }
+
 fn next_secret(secret: i128) -> i128 {
     let mut next_secret = secret;
     let mul64 = next_secret << 6;
@@ -59,6 +65,16 @@ fn nth_secret(n: usize, secret: &i128) -> i128 {
     nth_secret
 }
 
+fn gen_n_secrets(n: usize, secret: &i128) -> Vec<i128> {
+    let mut nth_secret = *secret;
+    let mut secrets = Vec::new();
+    for _i in 0..n + 1 {
+        secrets.push(nth_secret);
+        nth_secret = next_secret(nth_secret);
+    }
+    secrets
+}
+
 fn solve_one(input: &Input) -> Result<Answer> {
     let Input { secret_nums } = input;
     let answer = secret_nums.iter().map(|s| nth_secret(2000, s)).sum();
@@ -66,8 +82,61 @@ fn solve_one(input: &Input) -> Result<Answer> {
 }
 
 fn solve_two(input: &Input) -> Result<Answer> {
-    let _unused = input;
-    Ok(Answer::Num(0))
+    let Input { secret_nums } = input;
+    let buyers: Vec<Buyer> = secret_nums.iter().map(|s| Buyer::from_secret(*s)).collect();
+    let answer = Buyer::best_sequence(&buyers);
+    Ok(Answer::Num(answer.0))
+}
+
+struct Buyer {
+    // secrets: Vec<i128>,
+    // prices: Vec<i128>,
+    // deltas: Vec<i128>,
+    sequence_prices: HashMap<[i128; 4], i128>,
+}
+
+impl Buyer {
+    fn from_secret(secret: i128) -> Self {
+        let secrets = gen_n_secrets(2000, &secret);
+        let prices: Vec<i128> = secrets.iter().map(|s| s % 10).collect();
+        let deltas: Vec<i128> = prices.windows(2).map(|w| w[1] - w[0]).collect();
+        let mut sequence_prices = HashMap::new();
+        for (s, price) in deltas.windows(4).zip(prices.iter().skip(4)) {
+            let k = [s[0], s[1], s[2], s[3]];
+            if !sequence_prices.contains_key(&k) {
+                sequence_prices.insert(k, *price);
+            }
+        }
+        Self {
+            // secrets,
+            // prices,
+            // deltas,
+            sequence_prices,
+        }
+    }
+    fn get_price(&self, sequence: &[i128; 4]) -> i128 {
+        *self.sequence_prices.get(sequence).unwrap_or(&0)
+    }
+    fn best_sequence(buyers: &[Buyer]) -> (i128, String) {
+        let mut check_sequences: HashSet<[i128; 4]> = HashSet::new();
+        for buyer in buyers.iter() {
+            check_sequences.extend(buyer.sequence_prices.keys());
+        }
+        let (most_bananas, best_sequence) = check_sequences
+            .iter()
+            .map(|s| (buyers.iter().map(|b| b.get_price(s)).sum::<i128>(), s))
+            .max_by(|a, b| a.0.cmp(&b.0))
+            .unwrap();
+        // println!("sequence: {:?}, bananas: {}", best_sequence, most_bananas);
+        (
+            most_bananas,
+            best_sequence
+                .iter()
+                .map(|s| s.to_string())
+                .collect::<Vec<_>>()
+                .join(","),
+        )
+    }
 }
 
 // Quickly obtain answers by running
@@ -81,6 +150,7 @@ mod day22_tests {
     use test::Bencher;
 
     static TEST: LazyLock<String> = local_file!("test");
+    static TEST2: LazyLock<String> = local_file!("test2");
     static INPUT: LazyLock<String> = local_file!("input");
 
     #[test]
@@ -116,13 +186,13 @@ mod day22_tests {
     }
     #[test]
     fn test_two() -> Result<()> {
-        let answer = super::part_two(&TEST)?;
-        assert_eq!(answer, Answer::Num(0));
+        let answer = super::part_two(&TEST2)?;
+        assert_eq!(answer, Answer::Num(23));
         Ok(())
     }
     fn part_two_impl() -> Result<()> {
         let answer = super::part_two(&INPUT)?;
-        assert_eq!(answer, Answer::Num(0));
+        assert_eq!(answer, Answer::Num(1896));
         Ok(())
     }
     #[bench]
