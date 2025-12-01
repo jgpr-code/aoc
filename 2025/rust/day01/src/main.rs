8ad197901e939a94e8b6bcdf280958643b5b1a3c
Jörg Planner <j.planner@gmx.de>
2025-12-01 22:44:50 +0100

    feat(2025/rust): day01 part1

--- 2025/rust/day01/src/main.rs
+++ 2025/rust/day01/src/main.rs
@@ -1,7 +1,9 @@
 #![feature(test)]
 extern crate test;
 
+use anyhow::anyhow;
 use anyhow::Result;
+use common::regx;
 use common::Answer;
 use std::io;
 
@@ -22,22 +24,66 @@ pub fn part_two(input: &str) -> Result<Answer> {
     solve_two(&input)
 }
 
+enum Rotation {
+    Left(u128),
+    Right(u128),
+}
+
+impl TryFrom<&str> for Rotation {
+    type Error = anyhow::Error;
+
+    fn try_from(value: &str) -> std::result::Result<Self, Self::Error> {
+        let re = regx!(r"([LR])(\d+)");
+        let caps = re.captures(value).ok_or(anyhow!("regex did not match"))?;
+        // unwraps and unreachable are fine because otherwise the regex wouldn't match
+        let rotation_type = caps.get(1).unwrap().as_str();
+        let amount = u128::from_str_radix(caps.get(2).unwrap().as_str(), 10)?;
+        Ok(match rotation_type {
+            "L" => Rotation::Left(amount),
+            "R" => Rotation::Right(amount),
+            _ => unreachable!(),
+        })
+    }
+}
+
 struct Input {
-    nums: Vec<i128>,
+    rotations: Vec<Rotation>,
 }
 
 fn parse_input(input: &str) -> Result<Input> {
     // example to collect Vec<Result<T, E>> to Result<Vec<T>, E>
-    let nums: Vec<i128> = input
+    let rotations: Vec<Rotation> = input
         .lines()
-        .map(|l| i128::from_str_radix(l, 10))
+        .map(|l| Rotation::try_from(l))
         .collect::<Result<Vec<_>, _>>()?;
-    Ok(Input { nums })
+    Ok(Input { rotations })
 }
 
 fn solve_one(input: &Input) -> Result<Answer> {
-    let Input { nums } = input;
-    Ok(Answer::Num(nums.iter().sum()))
+    let dial_size = 100;
+    let Input { rotations } = input;
+    let mut zero_counter = 0;
+    let mut dial_position = 50;
+    for rotation in rotations.iter() {
+        dial_position = match rotation {
+            Rotation::Left(amount) => {
+                let amount = amount % dial_size;
+                let mut new_dial_position = dial_position - amount as i128;
+                while new_dial_position < 0 {
+                    new_dial_position += dial_size as i128;
+                }
+                new_dial_position
+            }
+            Rotation::Right(amount) => {
+                let amount = amount % dial_size;
+                (dial_position + amount as i128) % 100
+            }
+        };
+        if dial_position == 0 {
+            zero_counter += 1;
+        }
+    }
+    Ok(Answer::Num(zero_counter))
 }
 
 fn solve_two(input: &Input) -> Result<Answer> {
@@ -61,12 +107,12 @@ mod day01_tests {
     #[test]
     fn test_one() -> Result<()> {
         let answer = super::part_one(&TEST)?;
-        assert_eq!(answer, Answer::Num(0));
+        assert_eq!(answer, Answer::Num(3));
         Ok(())
     }
     fn part_one_impl() -> Result<()> {
         let answer = super::part_one(&INPUT)?;
-        assert_eq!(answer, Answer::Num(0));
+        assert_eq!(answer, Answer::Num(984));
         Ok(())
     }
     #[bench]
