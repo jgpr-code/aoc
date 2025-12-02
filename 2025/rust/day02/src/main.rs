5c687b277ae513683059e7d92dc5f57b47a263e1
Jörg Planner <j.planner@gmx.de>
2025-12-02 22:25:19 +0100

    feat(2025/rust): day02 part1

--- 2025/rust/day02/src/main.rs
+++ 2025/rust/day02/src/main.rs
@@ -1,6 +1,7 @@
 #![feature(test)]
 extern crate test;
 
+use anyhow::anyhow;
 use anyhow::Result;
 use common::Answer;
 use std::io;
@@ -23,21 +24,56 @@ pub fn part_two(input: &str) -> Result<Answer> {
 }
 
 struct Input {
-    nums: Vec<i128>,
+    ranges: Vec<(i128, i128)>,
 }
 
 fn parse_input(input: &str) -> Result<Input> {
-    // example to collect Vec<Result<T, E>> to Result<Vec<T>, E>
-    let nums: Vec<i128> = input
-        .lines()
-        .map(|l| i128::from_str_radix(l, 10))
+    let input = sanitize_newlines(input);
+    let ranges: Vec<(i128, i128)> = input
+        .split(',')
+        .map(|range| parse_range(range))
         .collect::<Result<Vec<_>, _>>()?;
-    Ok(Input { nums })
+    Ok(Input { ranges })
+}
+
+fn sanitize_newlines(input: &str) -> String {
+    input.replace(|c| c == '\r' || c == '\n', "")
+}
+
+fn parse_range(range: &str) -> Result<(i128, i128)> {
+    range
+        .split_once('-')
+        .ok_or(anyhow!("range without: -"))
+        .and_then(|(from, to)| {
+            Ok((
+                i128::from_str_radix(from, 10)?,
+                i128::from_str_radix(to, 10)?,
+            ))
+        })
 }
 
 fn solve_one(input: &Input) -> Result<Answer> {
-    let Input { nums } = input;
-    Ok(Answer::Num(nums.iter().sum()))
+    let Input { ranges } = input;
+    Ok(Answer::Num(
+        ranges.iter().map(|range| sum_invalid_ids(*range)).sum(),
+    ))
+}
+
+fn sum_invalid_ids(range: (i128, i128)) -> i128 {
+    (range.0..=range.1)
+        .filter(|id| is_repeated_twice(*id))
+        .sum()
+}
+
+fn is_repeated_twice(number: i128) -> bool {
+    let digit_count = number.ilog10() + 1;
+    if digit_count % 2 == 1 {
+        return false;
+    }
+    let divisor = 10i128.pow(digit_count / 2);
+    let first_part = number / divisor;
+    let second_part = number % divisor;
+    return first_part == second_part;
 }
 
 fn solve_two(input: &Input) -> Result<Answer> {
@@ -61,12 +97,12 @@ mod day02_tests {
     #[test]
     fn test_one() -> Result<()> {
         let answer = super::part_one(&TEST)?;
-        assert_eq!(answer, Answer::Num(0));
+        assert_eq!(answer, Answer::Num(1227775554));
         Ok(())
     }
     fn part_one_impl() -> Result<()> {
         let answer = super::part_one(&INPUT)?;
-        assert_eq!(answer, Answer::Num(0));
+        assert_eq!(answer, Answer::Num(44854383294));
         Ok(())
     }
     #[bench]
